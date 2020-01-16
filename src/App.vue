a4dddc60ce6af2bc76ab14efd2ac88d0d43e702d
DavidSantander <santander.david.19@gmail.com>
2020-01-16 02:54:57 -0600

    Remove filter that changes the image and color of btns

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,15 @@
 <template>
-  <v-app>
-    <core-filter />
-
-    <core-toolbar />
-
-    <core-drawer />
-
-    <core-view />
-  </v-app>
+    <v-app>
+        <core-toolbar />
+        <core-drawer />
+        <core-view />
+    </v-app>
 </template>
 
 <style lang="scss">
 @import "@/styles/index.scss";
-
 /* Remove in 1.2 */
 .v-datatable thead th.column.sortable i {
-  vertical-align: unset;
+    vertical-align: unset;
 }
 </style>
